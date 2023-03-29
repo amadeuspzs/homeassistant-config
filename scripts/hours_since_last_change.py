f36b8647b457461c236591b3e298919aac748e3c
Amadeus <amadeus.stevenson@gmail.com>
2023-03-29 06:41:18 +0100

    feat: stale sensors

--- scripts/hours_since_last_change.py
+++ scripts/hours_since_last_change.py
@@ -14,7 +14,7 @@ def find_last_changed_index(df: pd.DataFrame) -> int:
     # first value is different to second
     elif df.state[0] != df.state[1]:
         last_index = 0
-    # fin last value
+    # find last value
     else:
         last_index = df[df.state.diff().bfill() != 0].dropna().index[0] - 1
     return last_index
@@ -22,12 +22,8 @@ def find_last_changed_index(df: pd.DataFrame) -> int:
 # define your home assistant db location below:
 db = "/home/homeassistant/.homeassistant/home-assistant_v2.db"
 
-# specify entity_id as first argument to this script
-try:
-    entity_id = sys.argv[1]
-except:
-    print("Usage: hours_since_last_change.py sensor.name")
-    exit(1)
+# how many restart events can we expect?
+num_restarts = 100
 
 # connect using SQLalchemy
 try:
@@ -36,31 +32,37 @@ try:
 except Exception as e:
     raise Exception(f"Could not connect to HA DB: {e}")
 
-# how many restart events can we expect?
-num_restarts = 100
+def hours_since_last_change(entity_id: str) -> float:
 
-# build the query
-query = f"""
-SELECT
-    state,
-    last_updated
-FROM states 
-WHERE 
-    entity_id="{entity_id}" AND
-    state !="unknown"
-ORDER BY last_updated DESC 
-LIMIT {num_restarts}
-"""
+    # build the query
+    query = f"""
+    SELECT
+        state,
+        last_updated
+    FROM states 
+    WHERE 
+        entity_id="{entity_id}" AND
+        state !="unknown"
+    ORDER BY last_updated DESC 
+    LIMIT {num_restarts}
+    """
 
-# execute the query and load into a pandas dataframe:
-try:
-    df = pd.read_sql_query(query, conn)
-    df.state = pd.to_numeric(df.state)
-except Exception as e:
-    raise Exception(f"Failed to query HA DB: {e}")
+    # execute the query and load into a pandas dataframe:
+    try:
+        df = pd.read_sql_query(query, conn)
+        df.state = pd.to_numeric(df.state)
+    except Exception as e:
+        raise Exception(f"Failed to query HA DB: {e}")
 
-try:
-    last_updated = pd.to_datetime(df.iloc[find_last_changed_index(df)].last_updated)
-    print(round((pd.Timedelta(pd.Timestamp.now() - last_updated))/pd.Timedelta('1 hour'),1))
-except Exception as e:
-    raise Exception(f"Failed to find last state: {e}")
+    try:
+        last_updated = pd.to_datetime(df.iloc[find_last_changed_index(df)].last_updated)
+        return (round((pd.Timedelta(pd.Timestamp.now() - last_updated))/pd.Timedelta('1 hour'),1))
+    except Exception as e:
+        raise Exception(f"Failed to find last state: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} sensor_name")
+        exit(1)
+    entity_id = sys.argv[1]
+    print(hours_since_last_change(entity_id))
